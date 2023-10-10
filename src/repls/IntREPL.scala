ecdfb46ce87f4569b90e965123ca5f414b4a1f45
Aaron-Serpilin <serpilin.aaron@gmail.com>
2023-10-10 19:51:52 +0200

    Variable Assignment Functionality Added

--- src/repls/IntREPL.scala
+++ src/repls/IntREPL.scala
@@ -5,9 +5,30 @@ class IntREPL extends REPLBase {
 
     type Base = Int
     override val replName: String = "int-repl"
+    private val variables = mutable.Map[String, Int]() // Dictionary to store variables and their values
+
+    def isOperator(char: String): Boolean = Set("+", "-", "*", "/").contains(char)
+
+    def isInteger(char: String): Boolean = char.matches("-?\\d+")
+
+    def precedence(operator: String): Int = operator match {
+        case "+" | "-" => 1
+        case "*" | "/" => 2
+        case _ => 0 // Default precedence
+    }
+
+    def applyOperation(operand1: Int, operator: String, operand2: Int): Int = {
+        operator match {
+            case "+" => operand1 + operand2
+            case "-" => operand1 - operand2
+            case "*" => operand1 * operand2
+            case "/" => operand1 / operand2
+            case _ => throw new IllegalArgumentException(s"Invalid operator: $operator")
+        }
+    }
 
     // Shunting Yard Algorithm Pseudocode: https://aquarchitect.github.io/swift-algorithm-club/Shunting%20Yard/
-    private def normalToReversePolish(expression: Seq[String]): Seq[String] = {
+    private def expressionToRPN (expression: Seq[String]): Seq[String] = {
         val outputStack = mutable.Stack[String]()
         val operatorStack = mutable.Stack[String]()
 
@@ -48,7 +69,7 @@ class IntREPL extends REPLBase {
     }
 
     // Polish to Expression Tree Code: https://gitlab.com/vu-oofp/lecture-code/-/blob/master/OOReversePolish.scala
-    private def reversePolishToExpressionTree(expression: Seq[String]): String = {
+    private def RPNToTree(expression: Seq[String]): String = {
         val outputStack = mutable.Stack[String]()
         expression.foreach {
             case token if isOperator(token) =>
@@ -66,38 +87,36 @@ class IntREPL extends REPLBase {
         outputStack.top
     }
 
+    private def substituteVariables(expression: Seq[String]): Seq[String] = {
+        expression.map {
+            case variable if variables.contains(variable) =>
+                variables(variable).toString
+            case other =>
+                other
+        }
+    }
+
     override def readEval(command: String): String = {
         val expression = SplitExpressionString.splitExpressionString(command)
-        val reversePolishExpression = normalToReversePolish(expression)
-        val standardExpression = reversePolishToExpressionTree(reversePolishExpression)
-        standardExpression
-    }
 
-    def evaluateExpression(expression: Seq[String]): String = {
-        val operandStack = mutable.Stack[Int]() // Stack to store operands
-        val operatorStack = mutable.Stack[String]() // Stack to store operators
+        if (expression.length >= 3 && expression(1) == "=") {
+            // Extract variable name and expression
+            val variableName = expression.head
+            val variableExpression = expression.drop(2)
 
-        ""
-    }
+            val substitutedExpression = substituteVariables(variableExpression)
+            val reversePolishExpression = expressionToRPN(substitutedExpression)
+            val result = RPNToTree(reversePolishExpression)
+            variables(variableName) = result.toInt
+            s"$variableName = $result"
 
-    def isOperator(char: String): Boolean = Set("+", "-", "*", "/").contains(char)
+        } else {
 
-    def isInteger(char: String): Boolean = char.matches("-?\\d+")
+            val substitutedExpression = substituteVariables(expression)
+            val reversePolishExpression = expressionToRPN(substitutedExpression)
+            val result = RPNToTree(reversePolishExpression)
+            if (variables.contains(result)) s"$result = ${variables(result)}" else result
 
-    def precedence(operator: String): Int = operator match {
-        case "+" | "-" => 1
-        case "*" | "/" => 2
-        case _ => 0 // Default precedence
-    }
-
-    def applyOperation(operand1: Int, operator: String, operand2: Int): Int = {
-        operator match {
-            case "+" => operand1 + operand2
-            case "-" => operand1 - operand2
-            case "*" => operand1 * operand2
-            case "/" => operand1 / operand2
-            case _ => throw new IllegalArgumentException(s"Invalid operator: $operator")
         }
     }
 }
-
